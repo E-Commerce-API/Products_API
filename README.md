92917c4f26d6845b26b5f9669897b83f0bb613fb
Scott Provence <scott.provence@yahoo.com>
2021-07-02 20:22:05 -0400

    Readme update and cleanup

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# ProductsAPI
+
+# System Design Capstone
+
+The goal was to replace an existing API with a back end system that can support the full data set for
+an E-commerce site that can scale to meet the demands of production traffic.
+
+## Tech Stack
+
+**Server:** [Node](https://nodejs.org/en/), [Express](https://expressjs.com/)
+
+**Database:** [MongoDB](https://www.mongodb.com/), [Mongoose](https://mongoosejs.com/)
+
+**Production Deployment:** 3x AWS EC2 (t2.micro) instances - 20.04 Ubuntu - 8GB SSD
+
+**Development Stress Testing:** [artillery.io](https://artillery.io/), [New Relic](https://newrelic.com/)
+
+**Production Stress Testing:** [loader.io](https://loader.io/)
+
+## Optimizations
+
+Implemented MongoDB [pipeline aggregation](https://docs.mongodb.com/manual/core/aggregation-pipeline/) consolidating collections to a single collection reducing complicated queries.
+
+Utilized AWS load balancer to increase traffic up to 1600 requests per second while maintaining over 99% success rate.
+
+## Testing Results
+
+![Imgur](https://i.imgur.com/23QHyHP.png)
+
+![Imgur](https://i.imgur.com/BC7kKSt.png)
+
+## Author
+
+- [Scott Provence](https://github.com/scopro220/)
+
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,75 +1,20 @@
-let Product = require('../models/Products');
 let AllProducts = require('../models/AllProducts');
 let AllStyles = require('../models/AllStyles');
-let Styles = require('../models/Styles');
-let Photos = require('../models/Photos');
-let Skus = require('../models/Skus');
-let Features = require('../models/Features');
 let RelatedProducts = require('../models/RelatedProducts');
 
 module.exports = (app) => {
 
   app.get('/products/:product_id', async (req, res) => {
     const id = req.params.product_id;
-    // const prod = await Product.find({"id": id}, {"_id": 0})
-    // const feature = await Features.find({"product_id": Number(id)}, {"_id": 0, "id": 0, "product_id": 0})
-
-    // const final = [{
-    //   "id": prod[0].id,
-    //   "name": prod[0].name,
-    //   "slogan": prod[0].slogan,
-    //   "description": prod[0].description,
-    //   "category": prod[0].category,
-    //   "default_price": prod[0].default_price,
-    //   "features": feature
-    // }]
-    // res.send(final);
     const prod = await AllProducts.find(
       {"id": id},
       {"_id": 0, "features._id": 0, "features.id": 0, "features.product_id": 0}
     );
     res.send(prod);
-
   });
 
   app.get('/products/:product_id/styles', async (req, res) => {
     const id = req.params.product_id;
-
-    // const finalData = {
-    //   product_id: id,
-    //   results: []
-    // };
-    // await Styles.find({"productId": id}, {"_id": 0, "productId": 0})
-    //   .then((styles) => {
-    //     styles.map(async (style) => {
-    //       await Photos.find({"styleId": style.id}, {"_id": 0, "id": 0, "styleId": 0})
-    //         .then(async (photo) => {
-    //           await Skus.find({"styleId": style.id})
-    //             .then((skus) => {
-    //               const data = {
-    //                 "style_id": style.id,
-    //                 "name": style.name,
-    //                 "original_price": (style.original_price),
-    //                 "sale_price": (style.sale_price) || null,
-    //                 "default?": style['default_style'] === 1 ? true : false,
-    //                 "photos": photo,
-    //                 "skus": {}
-    //               }
-    //               skus.forEach(sku => {
-    //                 let key = sku.id;
-    //                 data.skus[key] = {
-    //                   quantity: sku.quantity,
-    //                   size: sku.size
-    //                 }
-    //               })
-    //               finalData.results.push(data)
-    //             })
-    //         })
-    //         if (finalData.results.length === styles.length){
-    //           res.send(finalData);
-    //         }
-    //       })
-    //   })
 
     const data = await AllStyles.find(
       {"productId": id},
@@ -100,10 +45,7 @@ module.exports = (app) => {
       })
       dataStructure.results.push(alldata);
     })
-
-
     res.send(dataStructure);
-
   });
 
   app.get('/products/:product_id/related', async (req, res) => {
